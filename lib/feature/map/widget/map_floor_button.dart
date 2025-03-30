823b92c8ed1cb79c617f2e181abbe5fdcd8f9522
Toranosuke Minamikawa <tigertora7571@gmail.com>
2025-03-30 20:43:38 +0900

    fix: lints

--- lib/feature/map/widget/map_floor_button.dart
+++ lib/feature/map/widget/map_floor_button.dart
@@ -6,15 +6,7 @@ import 'package:dotto/feature/map/domain/map_detail.dart';
 class MapFloorButton extends ConsumerWidget {
   const MapFloorButton({super.key});
 
-  static const List<String> floorBarString = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    'R6',
-    'R7'
-  ];
+  static const List<String> floorBarString = ['1', '2', '3', '4', '5', 'R6', 'R7'];
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -22,12 +14,9 @@ class MapFloorButton extends ConsumerWidget {
     final mapPageNotifier = ref.watch(mapPageProvider.notifier);
     final mapViewTransformationControllerProviderNotifier =
         ref.watch(mapViewTransformationControllerProvider.notifier);
-    final mapFocusMapDetailNotifier =
-        ref.watch(mapFocusMapDetailProvider.notifier);
-    TextStyle floorBarTextStyle =
-        const TextStyle(fontSize: 18.0, color: Colors.black87);
-    TextStyle floorBarSelectedTextStyle =
-        const TextStyle(fontSize: 18.0, color: customFunColor);
+    final mapFocusMapDetailNotifier = ref.watch(mapFocusMapDetailProvider.notifier);
+    TextStyle floorBarTextStyle = const TextStyle(fontSize: 18.0, color: Colors.black87);
+    TextStyle floorBarSelectedTextStyle = const TextStyle(fontSize: 18.0, color: customFunColor);
     // 350以下なら計算
     double floorButtonWidth = (MediaQuery.of(context).size.width - 30 < 350)
         ? MediaQuery.of(context).size.width - 30
@@ -39,7 +28,7 @@ class MapFloorButton extends ConsumerWidget {
         child: Align(
           alignment: Alignment.topCenter,
           child: Container(
-            color: Colors.grey.withOpacity(0.9),
+            color: Colors.grey.withValues(alpha: 0.9),
             width: floorButtonWidth,
             height: floorButtonHeight,
             // Providerから階数の変更を検知
@@ -52,20 +41,17 @@ class MapFloorButton extends ConsumerWidget {
                     child: Center(
                       child: TextButton(
                           style: TextButton.styleFrom(
-                            fixedSize:
-                                Size(floorButtonWidth / 7, floorButtonHeight),
-                            backgroundColor:
-                                (mapPage == i) ? Colors.black12 : null,
+                            fixedSize: Size(floorButtonWidth / 7, floorButtonHeight),
+                            backgroundColor: (mapPage == i) ? Colors.black12 : null,
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(0),
                             ),
                           ),
                           // 階数の変更をProviderに渡す
                           onPressed: () {
-                            mapViewTransformationControllerProviderNotifier
-                                    .state =
-                                TransformationController(Matrix4(1, 0, 0, 0, 0,
-                                    1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
+                            mapViewTransformationControllerProviderNotifier.state =
+                                TransformationController(
+                                    Matrix4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1));
                             mapPageNotifier.state = i;
                             mapFocusMapDetailNotifier.state = MapDetail.none;
                             FocusScope.of(context).unfocus();
@@ -73,9 +59,7 @@ class MapFloorButton extends ConsumerWidget {
                           child: Center(
                               child: Text(
                             floorBarString[i],
-                            style: (mapPage == i)
-                                ? floorBarSelectedTextStyle
-                                : floorBarTextStyle,
+                            style: (mapPage == i) ? floorBarSelectedTextStyle : floorBarTextStyle,
                           ))),
                     ),
                   ),
