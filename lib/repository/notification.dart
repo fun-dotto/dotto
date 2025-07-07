b0c59169f4c176bd4189bbe26b602ff0b8a37556
Kanta Oikawa <iam@kantacky.com>
2025-07-07 19:50:24 +0900

    Remove unnecessary print

--- lib/repository/notification.dart
+++ lib/repository/notification.dart
@@ -21,10 +21,10 @@ class NotificationRepository {
       provisional: false,
       sound: true,
     );
-    // final token = await messaging.getToken();
-    // if (token != null) {
-    //   debugPrint("FCM Token: $token");
-    // }
+    final token = await messaging.getToken();
+    if (token != null) {
+      debugPrint("FCM Token: $token");
+    }
   }
 
   Future<void> setupInteractedMessage(WidgetRef ref) async {
