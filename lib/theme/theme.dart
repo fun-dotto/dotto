0c53f79366fa93b9d82fdf231413cc23ad1ad496
Kanta Oikawa <iam@kantacky.com>
2025-07-07 17:02:28 +0900

    Theme (#213)
    * Add Dotto semantic color and temporary theme
    
    * Move current theme
    
    * Typography (#215)
    
    * Add Noto Sans JP font files
    
    * Add font to pubspec.yaml
    
    * Updata DottoTheme.lightTheme (temporary)

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
 import 'colors/theme_extension.dart';
+import 'package:dotto/components/color_fun.dart';
 
 class DottoTheme {
   static ThemeData get lightTheme {
-    return ThemeData.light().copyWith(
+    return ThemeData(
       colorScheme: const ColorScheme.light(),
+      fontFamily: 'NotoSansJP',
       extensions: [
         DottoSemanticColors.light,
       ],
+    );
+  }
+
+  static ThemeData get v1 {
+    return ThemeData(
+      primarySwatch: customFunColor,
+      colorScheme: ColorScheme.light(
+        primary: customFunColor,
+        onSurface: Colors.grey.shade900,
+        surface: Colors.grey.shade100,
+      ),
+      buttonTheme: ButtonThemeData(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.all(0),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+          padding: const WidgetStatePropertyAll(EdgeInsets.all(0)),
+          elevation: const WidgetStatePropertyAll(2),
+          surfaceTintColor: const WidgetStatePropertyAll(Colors.transparent),
+        ),
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: customFunColor,
+        foregroundColor: Colors.white,
+      ),
+      textButtonTheme: const TextButtonThemeData(
+        style: ButtonStyle(
+          padding: WidgetStatePropertyAll(EdgeInsets.all(0)),
+        ),
+      ),
+      dividerTheme: DividerThemeData(
+        color: Colors.grey.shade200,
+      ),
+      cardTheme: const CardTheme(
+        surfaceTintColor: Colors.white,
+      ),
+      fontFamily: 'Murecho',
     );
   }
 }
