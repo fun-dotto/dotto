03574ab008e42e95ffd715a885bd088cf2529310
Kanta Oikawa <iam@kantacky.com>
2025-07-07 19:36:40 +0900

    Rename MaterialApp title

--- lib/app.dart
+++ lib/app.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Project 03 Group C',
+      title: 'Dotto',
       theme: DottoTheme.v1,
       home: const BasePage(),
       localizationsDelegates: const [
