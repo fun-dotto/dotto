b68159cdb989d4a61321c20852b93042594821ea
ryka-1104 <b1022160@fun.ac.jp>
2025-07-14 17:32:41 +0900

    RemoteConfigKeys内のuserKeyとopinionKeyの修正

--- lib/domain/remote_config_keys.dart
+++ lib/domain/remote_config_keys.dart
@@ -2,6 +2,6 @@ class RemoteConfigKeys {
   static const String isDesignV2Enabled = 'is_design_v2_enabled';
   static const String isFunchEnabled = 'is_funch_enabled';
   static const String isValidAppVersion = 'is_valid_app_version';
-  static const String userKeySettingUrl = 'user_key_setting_url';
-  static const String opinionKeySettingUrl = 'opinion_key_setting_url';
+  static const String userKeySettingUrl = 'assignment_setup_url';
+  static const String opinionKeySettingUrl = 'feedback_form_url';
 }
