cf0b7b95d444cbeb59d82f86767ab4687e3829bb
ryka-1104 <b1022160@fun.ac.jp>
2025-07-14 17:56:25 +0900

    button.dartにボタン要素を追加

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,7 +1,17 @@
-// import 'package:dotto/importer.dart';
+import 'package:dotto/importer.dart';
 
-// class ContainedElevatedButtons {
-//   ElevatedButton customFunColor = ElevatedButton(
-//     onPressed: () {},
-//   );
-// }
+class ContainedElevatedButtons {
+  ElevatedButton containedButton = ElevatedButton(
+    onPressed: () {},
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Color(0xFF990000),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+    ),
+    child: Text(
+      "Button",
+      style: TextStyle(color: Colors.white),
+    ),
+  );
+}
