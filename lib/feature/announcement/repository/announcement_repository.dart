f1d5450b20bde8012611869e554cbebd5054a445
Kanta Oikawa <iam@kantacky.com>
2025-07-16 14:04:48 +0900

    Add AnnouncementRepository

---
+++ lib/feature/announcement/repository/announcement_repository.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:dotto/feature/announcement/domain/announcement.dart';
+import 'package:http/http.dart' as http;
+
+abstract class AnnouncementRepository {
+  Future<List<Announcement>> getAnnouncements(Uri url);
+}
+
+final class AnnouncementRepositoryImpl implements AnnouncementRepository {
+  @override
+  Future<List<Announcement>> getAnnouncements(Uri url) async {
+    final response = await http.get(url);
+    if (response.statusCode < 200 || response.statusCode > 299) {
+      return [];
+    }
+    final json = response.body;
+    final list = jsonDecode(json) as List<dynamic>;
+    final announcements = list
+        .map<Announcement>(
+            (e) => Announcement.fromJson(e as Map<String, dynamic>))
+        .toList();
+    return announcements;
+  }
+}
