33fedec62c9a5ea510916182d7e1170084115143
Kanta Oikawa <iam@kantacky.com>
2025-10-08 21:40:14 +0900

    Add TimetablePeriodStyleController

---
+++ lib/feature/timetable/controller/timetable_period_style_controller.dart
@@ -0,0 +1,25 @@
+import 'package:dotto/domain/user_preference_keys.dart';
+import 'package:dotto/feature/timetable/domain/timetable_period_style.dart';
+import 'package:dotto/repository/user_preference_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'timetable_period_style_controller.g.dart';
+
+@riverpod
+final class TimetablePeriodStyleNotifier
+    extends _$TimetablePeriodStyleNotifier {
+  @override
+  Future<TimetablePeriodStyle> build() async {
+    final styleString = await UserPreferenceRepository.getString(
+      UserPreferenceKeys.timetablePeriodStyle,
+    );
+    return TimetablePeriodStyle.fromString(styleString);
+  }
+
+  Future<void> setStyle(TimetablePeriodStyle style) async {
+    state = AsyncValue.data(style);
+    await UserPreferenceRepository.setString(
+      UserPreferenceKeys.timetablePeriodStyle,
+      style.toString(),
+    );
+  }
+}
