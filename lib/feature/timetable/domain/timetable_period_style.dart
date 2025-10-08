ef9c0c00145a413d5bd003aa164a2eee89949b3b
Kanta Oikawa <iam@kantacky.com>
2025-10-08 21:39:59 +0900

    Add a static method to TimetablePeriodStyle

--- lib/feature/timetable/domain/timetable_period_style.dart
+++ lib/feature/timetable/domain/timetable_period_style.dart
@@ -1,1 +1,17 @@
-enum TimetablePeriodStyle { numberOnly, timeOnly }
+enum TimetablePeriodStyle {
+  numberOnly,
+  timeOnly;
+
+  const TimetablePeriodStyle();
+
+  static TimetablePeriodStyle fromString(String? value) {
+    switch (value) {
+      case 'numberOnly':
+        return TimetablePeriodStyle.numberOnly;
+      case 'timeOnly':
+        return TimetablePeriodStyle.timeOnly;
+      default:
+        return TimetablePeriodStyle.numberOnly;
+    }
+  }
+}
