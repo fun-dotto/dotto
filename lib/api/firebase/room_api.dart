f71a8f83ae437d8f44515800d2b5f5776069bec6
Kanta Oikawa <iam@kantacky.com>
2025-11-13 13:49:42 +0900

    Add RoomAPI provider

--- lib/api/firebase/room_api.dart
+++ lib/api/firebase/room_api.dart
@@ -1,6 +1,9 @@
 import 'package:dotto/api/firebase/room_response.dart';
 import 'package:dotto/api/firebase/room_schedule_response.dart';
 import 'package:dotto/repository/firebase_realtime_database_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final roomApiProvider = Provider<RoomApi>((_) => _RoomApiImpl());
 
 abstract class RoomApi {
   Future<Map<String /* floor */, Map<String /* roomId */, RoomResponse>>>
@@ -9,7 +12,7 @@ abstract class RoomApi {
   getRoomSchedules();
 }
 
-final class RoomApiImpl implements RoomApi {
+final class _RoomApiImpl implements RoomApi {
   @override
   Future<Map<String, Map<String, RoomResponse>>> getRooms() async {
     final snapshot = await FirebaseRealtimeDatabaseRepository().getData('map');
