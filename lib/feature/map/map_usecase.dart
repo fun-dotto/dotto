fd6ad8963a28893f18e0b7d93e7ffe017148f0f1
Kanta Oikawa <iam@kantacky.com>
2025-11-13 23:09:46 +0900

    Update MapUseCase to use RoomTranslator

--- lib/feature/map/map_usecase.dart
+++ lib/feature/map/map_usecase.dart
@@ -1,7 +1,6 @@
 import 'package:dotto/api/firebase/room_api.dart';
-import 'package:dotto/domain/floor.dart';
 import 'package:dotto/domain/room.dart';
-import 'package:dotto/domain/room_schedule.dart';
+import 'package:dotto/translator/room_translator.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final class MapUseCase {
@@ -13,30 +12,6 @@ final class MapUseCase {
     final roomSchedulesResponse = await ref
         .read(roomApiProvider)
         .getRoomSchedules();
-    final rooms = roomsResponse.entries.map((entry) {
-      final floor = entry.key;
-      final rooms = entry.value;
-      return rooms.entries.map((roomEntry) {
-        final roomId = roomEntry.key;
-        final room = roomEntry.value;
-        final roomSchedules = roomSchedulesResponse[roomId] ?? [];
-        return Room(
-          id: roomId,
-          name: room.header,
-          description: room.detail ?? '',
-          floor: Floor.fromLabel(floor),
-          email: room.mail ?? '',
-          keywords: room.searchWordList ?? [],
-          schedules: roomSchedules.map((roomSchedule) {
-            return RoomSchedule(
-              beginDatetime: roomSchedule.beginDatetime,
-              endDatetime: roomSchedule.endDatetime,
-              title: roomSchedule.title,
-            );
-          }).toList(),
-        );
-      }).toList();
-    }).toList();
-    return rooms.expand((room) => room).toList();
+    return RoomTranslator.translate(roomsResponse, roomSchedulesResponse);
   }
 }
