f942f3763ad3a561ab55d3876c471baf27528fc1
Kanta Oikawa <iam@kantacky.com>
2025-11-14 21:48:38 +0900

    Update RoomAPI

--- lib/api/firebase/room_api.dart
+++ lib/api/firebase/room_api.dart
@@ -1,20 +1,9 @@
 import 'package:dotto/api/firebase/room_response.dart';
 import 'package:dotto/api/firebase/room_schedule_response.dart';
 import 'package:dotto/helper/firebase_realtime_database_repository.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final roomApiProvider = Provider<RoomApi>((_) => _RoomApiImpl());
-
-abstract class RoomApi {
-  Future<Map<String /* floor */, Map<String /* roomId */, RoomResponse>>>
-  getRooms();
-  Future<Map<String /* roomId */, List<RoomScheduleResponse>>>
-  getRoomSchedules();
-}
-
-final class _RoomApiImpl implements RoomApi {
-  @override
-  Future<Map<String, Map<String, RoomResponse>>> getRooms() async {
+final class RoomAPI {
+  static Future<Map<String, Map<String, RoomResponse>>> getRooms() async {
     final snapshot = await FirebaseRealtimeDatabaseRepository().getData('map');
     if (!snapshot.exists) {
       throw Exception('No data available');
@@ -44,8 +33,8 @@ final class _RoomApiImpl implements RoomApi {
     });
   }
 
-  @override
-  Future<Map<String, List<RoomScheduleResponse>>> getRoomSchedules() async {
+  static Future<Map<String, List<RoomScheduleResponse>>>
+  getRoomSchedules() async {
     final snapshot = await FirebaseRealtimeDatabaseRepository().getData(
       'map_room_schedule',
     );
