f65a4ffeb00c837e12990ace9dc277aefedecab0
Kanta Oikawa <iam@kantacky.com>
2025-11-14 21:48:57 +0900

    Update MapUseCase

--- lib/feature/map/map_usecase.dart
+++ lib/feature/map/map_usecase.dart
@@ -1,6 +1,5 @@
-import 'package:dotto/api/firebase/room_api.dart';
 import 'package:dotto/domain/room.dart';
-import 'package:dotto/translator/room_translator.dart';
+import 'package:dotto/repository/room_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final class MapUseCase {
@@ -8,10 +7,6 @@ final class MapUseCase {
   final Ref ref;
 
   Future<List<Room>> getRooms() async {
-    final roomsResponse = await ref.read(roomApiProvider).getRooms();
-    final roomSchedulesResponse = await ref
-        .read(roomApiProvider)
-        .getRoomSchedules();
-    return RoomTranslator.translate(roomsResponse, roomSchedulesResponse);
+    return ref.read(roomRepositoryProvider).getRooms();
   }
 }
