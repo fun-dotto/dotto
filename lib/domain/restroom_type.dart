3897d16fb3383f9847775307731835ed821f6438
Kanta Oikawa <iam@kantacky.com>
2025-11-15 19:56:52 +0900

    Update RestroomType

--- lib/domain/restroom_type.dart
+++ lib/domain/restroom_type.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 enum RestroomType {
   men(icon: Icons.man, color: Colors.blue),
   women(icon: Icons.woman, color: Colors.red),
-  wheelchair(icon: Icons.accessible, color: Colors.black),
+  multipurpose(icon: Icons.accessible, color: Colors.black),
   kitchenette(icon: Icons.countertops, color: Colors.black);
 
   const RestroomType({required this.icon, this.color});
