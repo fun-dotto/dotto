5f3d2b38690977cf892adc9e1a6eb048838160a4
Kanta Oikawa <iam@kantacky.com>
2025-11-15 19:50:08 +0900

    Add kitchenette to RestroomType

--- lib/domain/restroom_type.dart
+++ lib/domain/restroom_type.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 enum RestroomType {
   men(icon: Icons.man, color: Colors.blue),
   women(icon: Icons.woman, color: Colors.red),
-  wheelchair(icon: Icons.accessible);
+  wheelchair(icon: Icons.accessible, color: Colors.black),
+  kitchenette(icon: Icons.countertops, color: Colors.black);
 
   const RestroomType({required this.icon, this.color});
 
