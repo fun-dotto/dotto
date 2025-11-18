7270c02ced7f411dbdf257d6939124edabba328d
Kanta Oikawa <iam@kantacky.com>
2025-11-18 22:35:07 +0900

    Remove unused analytics related files

--- lib/domain/analytics_user_property_keys.dart
+++
@@ -1,7 +0,0 @@
-enum AnalyticsUserPropertyKeys {
-  grade(key: 'grade');
-
-  const AnalyticsUserPropertyKeys({required this.key});
-
-  final String key;
-}
--- lib/feature/timetable/controller/timetable_period_style_controller.dart
+++ lib/feature/timetable/controller/timetable_period_style_controller.dart
@@ -1,7 +1,6 @@
-import 'package:dotto/controller/analytics_controller.dart';
-import 'package:dotto/domain/analytics_event_keys.dart';
 import 'package:dotto/domain/user_preference_keys.dart';
 import 'package:dotto/feature/timetable/domain/timetable_period_style.dart';
+import 'package:dotto/helper/logger.dart';
 import 'package:dotto/helper/user_preference_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'timetable_period_style_controller.g.dart';
@@ -21,22 +20,11 @@ final class TimetablePeriodStyleNotifier
         ) ??
         TimetablePeriodStyle.numberOnly;
 
-    switch (style) {
-      case TimetablePeriodStyle.numberOnly:
-        await ref
-            .read(analyticsControllerProvider.notifier)
-            .logEvent(
-              key: AnalyticsEventKeys.timetablePeriodStyleBuiltWithNumberOnly,
-            );
-
-      case TimetablePeriodStyle.numberAndTime:
-        await ref
-            .read(analyticsControllerProvider.notifier)
-            .logEvent(
-              key:
-                  AnalyticsEventKeys.timetablePeriodStyleBuiltWithNumberAndTime,
-            );
-    }
+    await ref
+        .read(loggerProvider)
+        .logBuiltTimetableSetting(
+          isNumberOnly: style == TimetablePeriodStyle.numberOnly,
+        );
 
     return style;
   }
@@ -49,21 +37,10 @@ final class TimetablePeriodStyleNotifier
       style.key,
     );
 
-    switch (style) {
-      case TimetablePeriodStyle.numberOnly:
-        await ref
-            .read(analyticsControllerProvider.notifier)
-            .logEvent(
-              key: AnalyticsEventKeys.timetablePeriodStyleChangedToNumberOnly,
-            );
-
-      case TimetablePeriodStyle.numberAndTime:
-        await ref
-            .read(analyticsControllerProvider.notifier)
-            .logEvent(
-              key:
-                  AnalyticsEventKeys.timetablePeriodStyleChangedToNumberAndTime,
-            );
-    }
+    await ref
+        .read(loggerProvider)
+        .logSetTimetableSetting(
+          isNumberOnly: style == TimetablePeriodStyle.numberOnly,
+        );
   }
 }
