f0611cd0b79881f3fe11d87f73dbca18882f5ea0
Kanta Oikawa <iam@kantacky.com>
2025-11-20 00:44:15 +0900

    Add RootViewModelState

---
+++ lib/feature/root/root_viewmodel_state.dart
@@ -0,0 +1,10 @@
+import 'package:dotto/domain/tab_item.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'root_viewmodel_state.freezed.dart';
+
+@freezed
+abstract class RootViewModelState with _$RootViewModelState {
+  const factory RootViewModelState({required TabItem selectedTab}) =
+      _RootViewModelState;
+}
