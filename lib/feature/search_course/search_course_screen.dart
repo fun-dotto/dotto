8c5cc43f2b810f6d7e5f84a7bad37c358e5ad189
Kanta Oikawa <iam@kantacky.com>
2025-11-24 15:15:36 +0900

    Update SearchCourseScreen to have viewModel instance

--- lib/feature/search_course/search_course_screen.dart
+++ lib/feature/search_course/search_course_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dotto/feature/search_course/search_course_viewmodel.dart';
+import 'package:dotto/feature/search_course/search_course_viewmodel_state.dart';
 import 'package:dotto/feature/search_course/widget/search_course_action_buttons.dart';
 import 'package:dotto/feature/search_course/widget/search_course_box.dart';
 import 'package:dotto/feature/search_course/widget/search_course_filter_section.dart';
@@ -8,26 +10,39 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final class SearchCourseScreen extends ConsumerWidget {
   const SearchCourseScreen({super.key});
 
+  Widget _body(AsyncValue<SearchCourseViewModelState> viewModelAsync) {
+    switch (viewModelAsync) {
+      case AsyncData(:final value):
+        return GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () => value.focusNode.unfocus(),
+          child: const SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SearchCourseBox(),
+                SearchCourseFilterSection(),
+                SearchCourseActionButtons(),
+                SearchCourseResultSection(),
+              ],
+            ),
+          ),
+        );
+      case AsyncLoading():
+        return const Center(child: CircularProgressIndicator());
+      case AsyncError(:final error):
+        return Center(child: Text('Error: $error'));
+    }
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final viewModelAsync = ref.watch(searchCourseViewModelProvider);
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(title: const Text('科目'), centerTitle: false),
-      body: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: const SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SearchCourseBox(),
-              SearchCourseFilterSection(),
-              SearchCourseActionButtons(),
-              SearchCourseResultSection(),
-            ],
-          ),
-        ),
-      ),
+      body: _body(viewModelAsync),
     );
   }
 }
