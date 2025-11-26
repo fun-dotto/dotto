5ab61dea7ab41f613aec42b71b2d38a66d8b11ac
Kanta Oikawa <iam@kantacky.com>
2025-11-26 21:19:17 +0900

    Update AnnouncementScreen

--- lib/feature/announcement/announcement_screen.dart
+++ lib/feature/announcement/announcement_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dotto/feature/announcement/announcement_view_state.dart';
 import 'package:dotto/feature/announcement/announcement_viewmodel.dart';
 import 'package:dotto/feature/announcement/widget/announcement_list.dart';
 import 'package:flutter/material.dart';
@@ -6,17 +7,24 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final class AnnouncementScreen extends ConsumerWidget {
   const AnnouncementScreen({super.key});
 
+  Widget _body(AsyncValue<AnnouncementViewState> viewModelAsync) {
+    switch (viewModelAsync) {
+      case AsyncData(:final value):
+        return AnnouncementList(announcements: value.announcements);
+      case AsyncError(:final error):
+        return Center(child: Text('エラーが発生しました: $error'));
+      case AsyncLoading():
+        return const Center(child: CircularProgressIndicator());
+    }
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final viewModel = ref.watch(announcementViewModelProvider);
+    final viewModelAsync = ref.watch(announcementViewModelProvider);
 
     return Scaffold(
       appBar: AppBar(title: const Text('お知らせ')),
-      body: viewModel.when(
-        data: (data) => AnnouncementList(announcements: data.announcements),
-        error: (error, stackTrace) => const Center(child: Text('エラーが発生しました')),
-        loading: () => const Center(child: CircularProgressIndicator()),
-      ),
+      body: _body(viewModelAsync),
     );
   }
 }
