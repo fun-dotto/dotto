7d92c098fc3260c46dabad953f2c663c2ed84b77
Kanta Oikawa <iam@kantacky.com>
2025-11-26 21:30:13 +0900

    Announcement Repository をアップデート (#369)
    * Add OpenAPI schema
    
    * Generate from schema
    
    * Update pubspec
    
    * Add Taskfile to api
    
    * Update Taskfile
    
    * Fix Taskfile
    
    * Remove filter
    
    * Remove isActive property from Announcement
    
    * Add api_client
    
    * Add dio to pubspec
    
    * Update annoucement_repository to use api_client
    
    * Fix build error of announcements_controller
    
    * Remove debugPrint
    
    * Move files
    
    * Update DebugScreen to change API environment

--- lib/feature/debug/debug_view_model.dart
+++ lib/feature/debug/debug_view_model.dart
@@ -1,5 +1,4 @@
-import 'dart:async';
-
+import 'package:dotto/api/api_environment.dart';
 import 'package:dotto/feature/debug/debug_view_model_state.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -13,6 +12,7 @@ class DebugViewModel extends _$DebugViewModel {
   Future<DebugViewModelState> build() async {
     final appCheckAccessToken = await FirebaseAppCheck.instance.getToken();
     final idToken = await FirebaseAuth.instance.currentUser?.getIdToken();
+    await ref.read(apiEnvironmentProvider.notifier).load();
     return DebugViewModelState(
       appCheckAccessToken: appCheckAccessToken,
       idToken: idToken,
