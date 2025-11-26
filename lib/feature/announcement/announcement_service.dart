d9ba36d2e54c9364a4092cfcaf876f32e6df6198
Kanta Oikawa <iam@kantacky.com>
2025-11-26 21:30:43 +0900

    Announcement Service を追加 (#371)
    * Add OpenAPI schema
    
    * Generate from schema
    
    * Update pubspec
    
    * Add Taskfile to api
    
    * Update Taskfile
    
    * Fix Taskfile
    
    * Remove filter
    
    * Remove isActive property from Announcement
    
    * Add api_client
    
    * Add dio to pubspec
    
    * Update annoucement_repository to use api_client
    
    * Fix build error of announcements_controller
    
    * Remove debugPrint
    
    * Move files
    
    * Update DebugScreen to change API environment
    
    * Add AnnouncementService

---
+++ lib/feature/announcement/announcement_service.dart
@@ -0,0 +1,13 @@
+import 'package:dotto/domain/announcement.dart';
+import 'package:dotto/repository/announcement_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final class AnnouncementService {
+  AnnouncementService({required this.ref});
+
+  final Ref ref;
+
+  Future<List<Announcement>> getAnnouncements() async {
+    return ref.read(announcementRepositoryProvider).getAnnouncements();
+  }
+}
