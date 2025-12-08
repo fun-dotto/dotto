6a9edae3ddc5959755a8d3df90b57bc40bf7dacf
Ayaka Hosoda <ayaka.hosoda@icloud.com>
2025-12-08 17:27:38 +0900

    Create GitHubProfile type

---
+++ lib/domain/contributor.dart
@@ -0,0 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'contributor.freezed.dart';
+
+@freezed
+class GitHubProfile with _$GitHubProfile {
+  const factory GitHubProfile({
+    required String id,
+    required String login,
+    required String avatarUrl,
+    required String htmlUrl,
+  }) = _GitHubProfile;
+}
