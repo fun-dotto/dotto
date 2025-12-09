9f070680016c743b1eb1852cc4930de124b816f6
Ayaka Hosoda <ayaka.hosoda@icloud.com>
2025-12-09 15:57:23 +0900

    Create githubContributionRepositoryProvider

--- lib/repository/github_contoributor_repository.dart
+++ lib/repository/github_contoributor_repository.dart
@@ -1,4 +1,10 @@
 import 'package:dotto/domain/github_profile.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final githubContributionRepositoryProvider =
+    Provider<GitHubContributorRepository>(
+      (ref) => GitHubContributorRepositoryImpl(ref),
+    );
 
 abstract class GitHubContributorRepository {
   Future<List<GitHubProfile>> getContributors();
