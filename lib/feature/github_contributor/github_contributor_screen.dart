163063a2618798aaccbd28d29d05d3faf3860e31
Ayaka Hosoda <167756153+Hosoda-abo@users.noreply.github.com>
2025-12-25 20:40:51 +0900

    GitHubContributorScreenを作成 (#415)
    * Create GitHubContributorVieeModel
    
    * Create githubContributorViewModelTest
    
    * Create githubContributorScreen
    
    * Fix Copilot error
    
    * Transition from the Settings Screen to the GitHub Contributor Screen
    
    * Fix copilot error
    
    * Fix the classname GitHubContributorViewModelProvider
    
    * Display usename
    
    * Update lib/feature/setting/settings.dart
    
    Co-authored-by: Kanta Oikawa <iam@kantacky.com>
    
    * Refactor GithubContributorViewmodel to use a class-level service instance for improved code clarity and efficiency (#417)
    
    ---------
    
    Co-authored-by: Kanta Oikawa <iam@kantacky.com>

---
+++ lib/feature/github_contributor/github_contributor_screen.dart
@@ -0,0 +1,65 @@
+import 'package:dotto/domain/github_profile.dart';
+import 'package:dotto/feature/github_contributor/github_contributor_viewmodel.dart';
+import 'package:dotto/feature/github_contributor/github_contributor_viewstate.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+final class GitHubContributorScreen extends ConsumerWidget {
+  const GitHubContributorScreen({super.key});
+
+  Widget _githubContributorListRow(GitHubProfile githubProfile) {
+    return ListTile(
+      leading: GestureDetector(
+        child: CircleAvatar(
+          radius: 20,
+          backgroundImage: NetworkImage(githubProfile.avatarUrl),
+          backgroundColor: Colors.grey.shade200,
+        ),
+      ),
+      title: Text(githubProfile.login),
+      onTap: () => launchUrlString(githubProfile.htmlUrl),
+    );
+  }
+
+  Widget _body(
+    AsyncValue<GitHubContributorViewState> viewModelAsync, {
+    required Future<void> Function() onRefresh,
+  }) {
+    switch (viewModelAsync) {
+      case AsyncData(:final value):
+        return RefreshIndicator(
+          onRefresh: onRefresh,
+          child: ListView.separated(
+            itemCount: value.contributors.length,
+            separatorBuilder: (_, _) => const Divider(height: 0),
+            itemBuilder: (_, index) {
+              final contributor = value.contributors[index];
+              return _githubContributorListRow(contributor);
+            },
+          ),
+        );
+      case AsyncError(:final error):
+        return Center(child: Text('エラーが発生しました: $error'));
+      case AsyncLoading():
+        return const Center(child: CircularProgressIndicator());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewModelAsync = ref.watch(gitHubContributorViewModelProvider);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('開発者一覧')),
+      body: _body(
+        viewModelAsync,
+        onRefresh: () async {
+          await ref
+              .read(gitHubContributorViewModelProvider.notifier)
+              .onRefresh();
+        },
+      ),
+    );
+  }
+}
