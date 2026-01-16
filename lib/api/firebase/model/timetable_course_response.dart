20e07c407dd4dddbd4bbf53aa5843833fd550b48
Kanta Oikawa <iam@kantacky.com>
2026-01-16 18:48:58 +0900

    Add TimetableCourseResponse model for Firebase API
    - Introduced `TimetableCourseResponse` class with JSON serialization support using Freezed.
    - Added fields for lesson ID, title, resource IDs, and optional cancellation and supervision flags to enhance timetable data handling.

---
+++ lib/api/firebase/model/timetable_course_response.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'timetable_course_response.freezed.dart';
+part 'timetable_course_response.g.dart';
+
+@freezed
+abstract class TimetableCourseResponse with _$TimetableCourseResponse {
+  //
+  // ignore: invalid_annotation_target
+  @JsonSerializable(fieldRename: FieldRename.snake)
+  const factory TimetableCourseResponse({
+    required int lessonId,
+    required String title,
+    required List<int> resourseIds,
+    @Default(false) bool cancel,
+    @Default(false) bool sup,
+  }) = _TimetableCourseResponse;
+
+  factory TimetableCourseResponse.fromJson(Map<String, Object?> json) =>
+      _$TimetableCourseResponseFromJson(json);
+}
