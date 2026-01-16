dbee947be7e6588f9d76a2f1267f810da5957075
Kanta Oikawa <iam@kantacky.com>
2026-01-16 23:21:42 +0900

    Remove AppColor

--- lib/feature/bus/domain/bus_type.dart
+++ lib/feature/bus/domain/bus_type.dart
@@ -1,11 +1,11 @@
-import 'package:dotto/theme/v1/app_color.dart';
+import 'package:dotto_design_system/style/primitive_color.dart';
 import 'package:flutter/material.dart';
 
 enum BusType {
-  kameda('亀田支所', Colors.grey),
-  goryokaku('五稜郭方面', AppColor.dividerRed),
-  syowa('昭和方面', AppColor.linkTextBlue),
-  other('', Colors.grey);
+  kameda('亀田支所', PrimitiveColor.gray500),
+  goryokaku('五稜郭方面', PrimitiveColor.red500),
+  syowa('昭和方面', PrimitiveColor.blue500),
+  other('', PrimitiveColor.gray500);
 
   const BusType(this.where, this.dividerColor);
 
