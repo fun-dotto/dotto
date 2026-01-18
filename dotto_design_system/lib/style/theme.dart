1b25d58897e2b1362b821fcb7e965a63c6891c02
ryka-1104 <gyd90621@gmail.com>
2026-01-18 19:58:11 +0900

    fix_pull_requests

--- dotto_design_system/lib/style/theme.dart
+++ dotto_design_system/lib/style/theme.dart
@@ -5,21 +5,21 @@ final class DottoTheme {
   static ThemeData get v2 {
     return ThemeData(
       colorScheme: const ColorScheme.light(),
-      fontFamily: 'Inter',
+      fontFamily: 'NotoSansJP',
       extensions: const [SemanticColor.light],
       textTheme: const TextTheme(
         largeTitleRegular: TextStyle(fontWeight: FontWeight.w400, fontSize: 28),
         largeTitleEmphasized: TextStyle(
-          fontWeight: FontWeight.bold,
+          fontWeight: FontWeight.w500,
           fontSize: 28,
         ),
         headlineRegular: TextStyle(fontWeight: FontWeight.w400, fontSize: 24),
         titleRegular: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
-        titleEmphasized: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        titleEmphasized: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
         bodyRegular: TextStyle(fontWeight: FontWeight.w400, fontSize: 16),
-        bodyEmphasized: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        bodyEmphasized: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
         captionRegular: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
-        captionEmphasized: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+        captionEmphasized: TextStyle(fontWeight: FontWeight.w500, fontSize: 12),
       ),
     );
   }
