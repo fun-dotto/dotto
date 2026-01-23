73f3a3cb3aaac395d7e36fbd8930dedd0d736000
Kanta Oikawa <iam@kantacky.com>
2026-01-23 10:18:46 +0900

    Remove unused color and theme files to streamline the codebase and improve maintainability.

--- lib/theme/v1/app_color.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-final class AppColor {
-  static const Color linkTextBlue = Color(0xFF1976D2);
-  static const Color dividerRed = Color(0xFFE84536);
-  static const Color textWhite = Colors.white;
-  static const Color textBlack = Colors.black;
-  static const Color dividerGrey = Color.fromARGB(255, 128, 128, 128);
-}
