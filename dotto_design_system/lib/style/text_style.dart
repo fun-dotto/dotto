1fe32cadfb43132ef951fb9e243161efb3c96495
Kanta Oikawa <iam@kantacky.com>
2026-01-23 13:56:21 +0900

    Add DottoTextStyle class for consistent text styling across the application
    This new file defines various text styles, including display, headline, title, body, and label styles, to enhance typography consistency and accessibility in the design system.

---
+++ dotto_design_system/lib/style/text_style.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+abstract final class DottoTextStyle {
+  static const displayLarge = TextStyle(
+    fontSize: 57,
+    height: 64 / 57,
+    fontWeight: FontWeight.normal,
+    letterSpacing: -0.25,
+  );
+  static const displayMedium = TextStyle(
+    fontSize: 45,
+    height: 52 / 45,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0,
+  );
+  static const displaySmall = TextStyle(
+    fontSize: 36,
+    height: 44 / 36,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0,
+  );
+  static const headlineLarge = TextStyle(
+    fontSize: 32,
+    height: 40 / 32,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0,
+  );
+  static const headlineMedium = TextStyle(
+    fontSize: 28,
+    height: 36 / 28,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0,
+  );
+  static const headlineSmall = TextStyle(
+    fontSize: 24,
+    height: 32 / 24,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0,
+  );
+  static const titleLarge = TextStyle(
+    fontSize: 22,
+    height: 28 / 22,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0,
+  );
+  static const titleMedium = TextStyle(
+    fontSize: 16,
+    height: 24 / 16,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0.15,
+  );
+  static const titleSmall = TextStyle(
+    fontSize: 14,
+    height: 20 / 14,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0.1,
+  );
+  static const bodyLarge = TextStyle(
+    fontSize: 16,
+    height: 24 / 16,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0.5,
+  );
+  static const bodyMedium = TextStyle(
+    fontSize: 14,
+    height: 20 / 14,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0.25,
+  );
+  static const bodySmall = TextStyle(
+    fontSize: 12,
+    height: 16 / 12,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0.4,
+  );
+  static const labelLarge = TextStyle(
+    fontSize: 14,
+    height: 20 / 14,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0.1,
+  );
+  static const labelMedium = TextStyle(
+    fontSize: 12,
+    height: 16 / 12,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0.5,
+  );
+  static const labelSmall = TextStyle(
+    fontSize: 11,
+    height: 16 / 11,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 0.5,
+  );
+}
