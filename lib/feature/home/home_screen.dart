8953fb07ca28a73e787524ab7bcd9567a4be00d9
Kanta Oikawa <iam@kantacky.com>
2026-01-26 15:19:05 +0900

    Refactor HomeScreen to enhance file item handling and improve code readability
    This commit updates the HomeScreen by replacing the previous file name path implementation with a more structured fileItems list. The refactor simplifies the mapping of file items to their respective UI components, improving maintainability and clarity in the code.

--- lib/feature/home/home_screen.dart
+++ lib/feature/home/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:dotto/controller/config_controller.dart';
 import 'package:dotto/domain/quick_link.dart';
 import 'package:dotto/feature/home/component/bus_card.dart';
-import 'package:dotto/feature/home/component/funch_card.dart';
 import 'package:dotto/feature/home/component/file_grid.dart';
 import 'package:dotto/feature/home/component/file_tile.dart';
+import 'package:dotto/feature/home/component/funch_card.dart';
 import 'package:dotto/feature/home/component/link_grid.dart';
 import 'package:dotto/feature/home/component/timetable_buttons.dart';
 import 'package:dotto/feature/home/component/timetable_calendar_view.dart';
@@ -25,27 +25,26 @@ final class HomeScreen extends ConsumerWidget {
     final config = ref.watch(configProvider);
     final timetablePeriodStyle = ref.watch(timetablePeriodStyleProvider);
 
-    final fileNamePath = <String, String>{
-      '学年暦': config.officialCalendarPdfUrl,
-      '時間割 前期': config.timetable1PdfUrl,
-      '時間割 後期': config.timetable2PdfUrl,
-    };
+    final fileItems = <(String label, String url, IconData icon)>[
+      ('学年暦', config.officialCalendarPdfUrl, Icons.event_note),
+      ('時間割 前期', config.timetable1PdfUrl, Icons.calendar_month),
+      ('時間割 後期', config.timetable2PdfUrl, Icons.calendar_month),
+    ];
     final infoTiles = <Widget>[
-      ...fileNamePath.entries.map(
+      ...fileItems.map(
         (item) => FileTile(
-          title: item.key,
-          icon: Icons.picture_as_pdf,
           onPressed: () {
             Navigator.of(context).push(
               MaterialPageRoute<void>(
-                builder: (_) =>
-                    WebPdfViewer(url: item.value, filename: item.key),
+                builder: (_) => WebPdfViewer(url: item.$2, filename: item.$1),
                 settings: RouteSettings(
-                  name: '/home/web_pdf_viewer?url=${item.value}',
+                  name: '/home/web_pdf_viewer?url=${item.$2}',
                 ),
               ),
             );
           },
+          icon: item.$3,
+          title: item.$1,
         ),
       ),
     ];
