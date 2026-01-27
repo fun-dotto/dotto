c69cddc81e3694195084e959b8aef263bb7ee1b8
Kanta Oikawa <iam@kantacky.com>
2026-01-27 09:39:06 +0900

    Add CourseDB

---
+++ lib/api/db/course_db.dart
@@ -0,0 +1,30 @@
+import 'package:dotto/feature/search_course/repository/syllabus_database_config.dart';
+import 'package:sqflite/sqflite.dart';
+
+final class CourseDB {
+  /// 指定された授業IDでデータベースから授業情報を取得する
+  static Future<Map<String, dynamic>?> fetchDB(int lessonId) async {
+    final dbPath = await SyllabusDatabaseConfig().getDBPath();
+    final database = await openDatabase(dbPath);
+
+    final records = await database.rawQuery(
+      'SELECT LessonId, 過去問, 授業名 FROM sort where LessonId = ?',
+      [lessonId],
+    );
+    if (records.isEmpty) {
+      return null;
+    }
+    return records.first;
+  }
+
+  static Future<List<String>> getLessonNameList(List<int> lessonIdList) async {
+    final dbPath = await SyllabusDatabaseConfig().getDBPath();
+    final database = await openDatabase(dbPath);
+
+    final List<Map<String, dynamic>> records = await database.rawQuery(
+      'SELECT 授業名 FROM sort WHERE LessonId in (${lessonIdList.join(",")})',
+    );
+    final lessonNameList = records.map((e) => e['授業名'] as String).toList();
+    return lessonNameList;
+  }
+}
