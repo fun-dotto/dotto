757312e49cdb4b178ae7d4cc3c3acc5103ed7671
Kanta Oikawa <iam@kantacky.com>
2026-01-27 10:22:25 +0900

    Add methods for bus to HomeService

--- lib/feature/home/home_service.dart
+++ lib/feature/home/home_service.dart
@@ -1,4 +1,7 @@
 import 'package:dotto/domain/timetable.dart';
+import 'package:dotto/feature/bus/controller/bus_is_to_controller.dart';
+import 'package:dotto/feature/bus/controller/bus_polling_controller.dart';
+import 'package:dotto/helper/location_helper.dart';
 import 'package:dotto/repository/timetable_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,5 +12,24 @@ final class HomeService {
 
   Future<List<Timetable>> getTimetables() async {
     return ref.read(timetableRepositoryProvider).getTimetables();
+  }
+
+  void startBusPolling() {
+    ref.read(busPollingProvider.notifier).start();
+  }
+
+  // TODO: Refactor
+  Future<void> changeDirectionOnCurrentLocation() async {
+    final locationHelper = ref.read(locationHelperProvider);
+    final position = await locationHelper.determinePosition();
+    if (position != null) {
+      final latitude = position.latitude;
+      if (latitude > 41.838770 && latitude < 41.845295) {
+        final longitude = position.longitude;
+        if (longitude > 140.765061 && longitude < 140.770368) {
+          ref.read(busIsToProvider.notifier).toggle();
+        }
+      }
+    }
   }
 }
